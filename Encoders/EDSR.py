31c252ce085358bb64feb79806c13eec96276594
tayyabnasir22 <tayyabnasir22@gmail.com>
2025-10-18 15:17:57 +0800

    added EDSR encoder

---
+++ Encoders/EDSR.py
@@ -0,0 +1,62 @@
+from Components.EDSRUpsampler import EDSRUpsampler
+from Components.MeanShift import MeanShift
+from Components.ResBlock import ResBlock
+import torch.nn as nn
+
+class EDSR(nn.Module):
+    def default_conv(self, in_channels, out_channels, kernel_size, bias=True):
+        return nn.Conv2d(
+            in_channels, out_channels, kernel_size,
+            padding=(kernel_size//2), bias=bias)
+
+    def __init__(self, n_resblocks=16, n_feats=64, res_scale=1,
+                       scale=[2], no_upsampling=True, rgb_range=1, n_colors = 3):
+        super(EDSR, self).__init__()
+        self.no_upsampling = no_upsampling
+        n_resblocks = n_resblocks
+        n_feats = n_feats
+        kernel_size = 3
+        scale = scale[0]
+        act = nn.ReLU(True)
+        
+        self.sub_mean = MeanShift(rgb_range)
+        self.add_mean = MeanShift(rgb_range, sign=1)
+
+        # define head module
+        m_head = [self.default_conv(n_colors, n_feats, kernel_size)]
+
+        # define body module
+        m_body = [
+            ResBlock(
+                self.default_conv, n_feats, kernel_size, act=act, res_scale=res_scale
+            ) for _ in range(n_resblocks)
+        ]
+        m_body.append(self.default_conv(n_feats, n_feats, kernel_size))
+
+        self.head = nn.Sequential(*m_head)
+        self.body = nn.Sequential(*m_body)
+
+        if self.no_upsampling:
+            self.out_dim = n_feats
+        else:
+            self.out_dim = n_colors
+            # define tail module
+            m_tail = [
+                EDSRUpsampler(self.default_conv, scale, n_feats, act=False),
+                self.default_conv(n_feats, n_colors, kernel_size)
+            ]
+            self.tail = nn.Sequential(*m_tail)
+
+    def forward(self, x):
+        #x = self.sub_mean(x)
+        x = self.head(x)
+
+        res = self.body(x)
+        res += x
+
+        if self.no_upsampling:
+            x = res
+        else:
+            x = self.tail(res)
+        #x = self.add_mean(x)
+        return x
