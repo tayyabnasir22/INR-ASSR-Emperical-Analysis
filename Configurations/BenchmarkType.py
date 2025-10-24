6925c2f3e680a606401733d9a25f37a9b2640a6e
tayyabnasir22 <tayyabnasir22@gmail.com>
2025-10-24 12:11:27 +0800

    refactoring

--- Configurations/BenchmarkType.py
+++ Configurations/BenchmarkType.py
@@ -1,5 +1,5 @@
 from enum import Enum
 
-class BenchmarkTypes(Enum):
-    DIV2k = 'DIV2K'
+class BenchmarkType(Enum):
+    DIV2K = 'DIV2K'
     BENCHMARK = 'BENCHMARK'
--- Models/Timer.py
+++ Models/Timer.py
@@ -2,10 +2,10 @@ from time import time
 
 class Timer():
     def __init__(self):
-        self.v = time.time()
+        self.start = time.time()
 
-    def s(self):
-        self.v = time.time()
+    def Reset(self):
+        self.start = time.time()
 
-    def t(self):
-        return time.time() - self.v
+    def Elapsed(self):
+        return time.time() - self.start
