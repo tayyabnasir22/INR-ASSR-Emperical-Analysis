bcc28204bc7ccf83b962b3962a9b175a861815ca
Tayyab Nasir <tayyabnasir22@gmail.com>
2025-11-05 13:03:12 +0800

    added paired testing + bug fixes

---
+++ test.py
@@ -0,0 +1,7 @@
+from Validators.EDSR_LIIF_SimplePairedValidator import EDSR_LIIF_SimplePairedValidator
+
+def main():
+    EDSR_LIIF_SimplePairedValidator().TestModel()
+
+if __name__ == '__main__':
+    main()
