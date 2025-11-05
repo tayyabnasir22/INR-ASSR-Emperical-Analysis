7e27b77a537adf9ed7fd11d1640722658d8ac6ee
Tayyab Nasir <tayyabnasir22@gmail.com>
2025-11-05 13:21:10 +0800

    refactoring training and validation drivers

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # INR-ASSR-Emperical-Analysis
+
+Orechestrate existing Pipelines and create new ones inheriting the existing ones or defining your own.
--- Trainers/BaseTrainer.py
+++ Trainers/BaseTrainer.py
@@ -1,6 +1,20 @@
 from abc import ABC, abstractmethod
+from ModelFactories.BaseModelFactory import BaseModelFactory
+from Pipelines.BaseTrainingPipeline import BaseTrainingPipeline
+from Utilities.PathManager import PathManager
+from Utilities.TrainingHelpers import TrainingHelpers
 
 class BaseTrainer(ABC):
     @abstractmethod
     def TrainModel(self,):
         pass
+
+    def _RunTrain(self, pipeline: BaseTrainingPipeline, factory: BaseModelFactory):
+        factory.BuildModel(pipeline)
+
+        # 3. Call the training
+        logger, writer = PathManager.SetModelSavePath(
+            pipeline.configurations.save_path, False
+        )
+
+        TrainingHelpers.Train(pipeline, logger, writer, 0, False)
