464e6fc8ddd4b0bd3f3dc4b70a84e9a4da7674e9
Tayyab Nasir <tayyabnasir22@gmail.com>
2025-11-09 13:09:12 +0800

    RDN based training orchestrator

---
+++ TrainingOrchestrators/RDN_SRNO_Trainer.py
@@ -0,0 +1,66 @@
+from Models.DecoderType import DecoderType
+from Models.EncoderType import EncoderType
+from Models.RecipeType import RecipeType
+from Trainers.BaseTrainer import BaseTrainer
+
+class RDN_SRNO_Trainer:
+    @staticmethod
+    def TrainSimple():
+        BaseTrainer(
+            encoder=EncoderType.RDN,
+            decoder=DecoderType.SRNO,
+            recipe=RecipeType.Simple,
+            input_patch=48,
+            scale_range=[1, 4]
+        ).TrainModel()
+
+    @staticmethod
+    def TrainSimpleLargerPatch():
+        BaseTrainer(
+            encoder=EncoderType.RDN,
+            decoder=DecoderType.SRNO,
+            recipe=RecipeType.Simple,
+            input_patch=64,
+            scale_range=[1, 4]
+        ).TrainModel()
+
+    @staticmethod
+    def TrainSimpleLargerPatchScale():
+        BaseTrainer(
+            encoder=EncoderType.RDN,
+            decoder=DecoderType.SRNO,
+            recipe=RecipeType.Simple,
+            input_patch=64,
+            scale_range=[1, 6]
+        ).TrainModel()
+
+    @staticmethod
+    def TrainSGDR():
+        BaseTrainer(
+            encoder=EncoderType.RDN,
+            decoder=DecoderType.SRNO,
+            recipe=RecipeType.SGDR,
+            input_patch=48,
+            scale_range=[1, 4]
+        ).TrainModel()
+
+    @staticmethod
+    def TrainGradLoss():
+        BaseTrainer(
+            encoder=EncoderType.RDN,
+            decoder=DecoderType.SRNO,
+            recipe=RecipeType.GradLoss,
+            input_patch=48,
+            scale_range=[1, 4]
+        ).TrainModel()
+
+    @staticmethod
+    def TrainGramL1Loss():
+        BaseTrainer(
+            encoder=EncoderType.RDN,
+            decoder=DecoderType.SRNO,
+            recipe=RecipeType.GramL1Loss,
+            input_patch=48,
+            scale_range=[1, 4]
+        ).TrainModel()
+    
