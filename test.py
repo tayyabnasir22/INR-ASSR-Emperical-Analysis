3ea661fb2dab4bcade15463d85ab9636b86400de
Tayyab Nasir <tayyabnasir22@gmail.com>
2025-11-11 16:41:10 +0800

    Added multiple testing strategies

--- test.py
+++ test.py
@@ -8,6 +8,36 @@ def main():
         DecoderType.HIIF
     )
 
+    TestingOrchestrator.TestSet5(
+        EncoderType.EDSR,
+        DecoderType.HIIF
+    )
+
+    TestingOrchestrator.TestSet14(
+        EncoderType.EDSR,
+        DecoderType.HIIF
+    )
+
+    TestingOrchestrator.TestB100(
+        EncoderType.EDSR,
+        DecoderType.HIIF
+    )
+
+    TestingOrchestrator.TestUrban100(
+        EncoderType.EDSR,
+        DecoderType.HIIF
+    )
+
+    TestingOrchestrator.TestSVT(
+        EncoderType.EDSR,
+        DecoderType.HIIF
+    )
+
+    TestingOrchestrator.TestCelebA_HQ(
+        EncoderType.EDSR,
+        DecoderType.HIIF
+    )
+
 
 if __name__ == '__main__':
     main()
