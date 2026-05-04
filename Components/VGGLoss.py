cea6083b535f8e63602b1f35c7076b7a13322b98
Tayyab Nasir <tayyabnasir22@gmail.com>
2026-05-04 16:23:22 +0800

    added VGG loss

---
+++ Components/VGGLoss.py
@@ -0,0 +1,37 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torchvision import models, transforms
+
+class VGGLoss(nn.Module):
+    models = {'vgg16': models.vgg16, 'vgg19': models.vgg19}
+
+    def __init__(self, model='vgg16', layer=8, shift=0, reduction='mean'):
+        super().__init__()
+        self.shift = shift
+        self.reduction = reduction
+        self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                              std=[0.229, 0.224, 0.225])
+        self.model = self.models[model](pretrained=True).features[:layer+1]
+        self.model.eval()
+        self.model.requires_grad_(False)
+
+    def get_features(self, input):
+        return self.model(self.normalize(input))
+
+    def train(self, mode=True):
+        self.training = mode
+
+    def forward(self, input, target, target_is_features=False):
+        if target_is_features:
+            input_feats = self.get_features(input)
+            target_feats = target
+        else:
+            sep = input.shape[0]
+            batch = torch.cat([input, target])
+            if self.shift and self.training:
+                padded = F.pad(batch, [self.shift] * 4, mode='replicate')
+                batch = transforms.RandomCrop(batch.shape[2:])(padded)
+            feats = self.get_features(batch)
+            input_feats, target_feats = feats[:sep], feats[sep:]
+        return F.mse_loss(input_feats, target_feats, reduction=self.reduction)
